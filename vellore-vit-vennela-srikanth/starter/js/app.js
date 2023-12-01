79931991e9412539c818b591be34f901e37d4c34
baludu <vemulakotabaludu02@gmail.com>
2023-11-30 23:54:30 -0800

    project completed

---
+++ vellore-vit-vennela-srikanth/starter/js/app.js
@@ -0,0 +1,44 @@
+// DOM => document object model //
+
+function init() {
+    const hamburger = document.querySelector(".hamburger");
+
+    const sidebarCloseButton = document.querySelector(".close-button");
+    
+    
+    const sidebarContainer = document.querySelector(".sidebar-container");
+    
+    
+    const openSidebar = () => {
+        sidebarContainer.classList.add("show-sidebar");
+       
+    };
+    
+    const closeSidebar = () => {
+       sidebarContainer.classList.remove("show-sidebar");
+    };
+
+
+    hamburger.addEventListener("click", openSidebar);
+    sidebarCloseButton.addEventListener("click", closeSidebar);
+}
+
+init();
+
+function navCode() {
+const navbarContainer = document.querySelector(".nav-container");
+window.addEventListener("scroll", function () { 
+    const scrollNumber = window.scrollY;
+const targetNumber = window.innerwidth >= 992 ? 70 : 50;
+
+
+if (scrollNumber >= targetNumber) {
+    navbarContainer.classList.add("sticky-nav");
+} else {
+    navbarContainer.classList.remove("sticky-nav");
+}
+}) 
+
+}
+
+navCode();
